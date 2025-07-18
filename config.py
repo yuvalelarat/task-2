ab9fe4024b2a8648172edd8ab1227895d1793ccc
‪Yuval Elarat‬‏ <yuvalelarat@gmail.com>
2025-07-18 16:55:02 +0300

    removing useless code

--- config.py
+++ config.py
@@ -4,15 +4,3 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-
-# example_policy = {
-#     "Version": "2022-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "Action": "*",
-#             "Resource": "*"
-#         }
-#     ]
-# }
